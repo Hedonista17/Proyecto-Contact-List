b6637abaf17306c924e90970d840f5a317d314c3
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-06 22:03:20 +0000

    Primer muestrario

---
+++ src/js/component/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+
+
+    return (
+        
+        <nav className="navbar navbar-light bg-dark ">
+            <a className="navbar-brand text-light mx-4" target="_blank" href="https://github.com/Hedonista17">
+            <i className="fa-regular fa-address-card fa-2xl mx-4"></i>
+             Lista de Contactos - Proyecto React & Context
+            </a>
+            <div className="text-end text-light mx-4"> Created by   Andrea Fernández </div>
+        </nav>
+
+    )
+}
+
+export default Navbar;
---
+++ src/js/component/intro.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Bienvenida = () => {
+
+
+    return (
+        <>
+        <div class="jumbotron text-center">
+            <h1 class="display-4">Bienvenidos!</h1>
+            <p class="lead">Esto es un simple proyecto realizado con React</p>
+            <hr class="my-2" />
+
+            <p> Si deseas saber como funciona, simplemente tienes que hacer click aqui abajo. </p>
+        </div>
+        
+        
+        </>
+    );
+};
+
+
+
+export default Bienvenida;
